194db0b1dd1f4d8b1a0501173f651e2f07aebc59
Phongnirat <phongnirat.thi@gmail.com>
2025-11-23 13:32:21 +0700

    .

--- room-booking-backend/server.js
+++ room-booking-backend/server.js
@@ -51,6 +51,6 @@ io.on("connection", (socket) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
 
 module.exports = io;
